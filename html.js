7809f264f9be34de03d3c902cbbd958325f55877
smt-ivr <s6754091@gmail.com>
2026-05-31 14:31:07 +0300

    Update html.js

--- html.js
+++ html.js
@@ -17,7 +17,7 @@ export const htmlContent = `<!DOCTYPE html>
                     <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
                     <circle cx="12" cy="7" r="4"></circle>
                 </svg>
-                <span data-i18n="personalArea">אזור אישי מתרימים</span>
+                <span data-i18n="personalArea">מתרימים</span>
             </a>
 
             <div class="lang-toggle-inline">
